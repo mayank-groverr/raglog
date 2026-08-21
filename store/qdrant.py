e31fd5e54bf338e9da7738adb4ab6a98f6ffffa6
DevNishantHub <nishantkumar211106@gmail.com>
2026-08-21 20:38:07 +0530

    chore: Added Create Collection Function

--- store/qdrant.py
+++ store/qdrant.py
@@ -3,15 +3,9 @@ from qdrant_client.models import Distance, VectorParams
 
 client = QdrantClient(host="localhost", port=6333)
 
-client.create_collection(
-    collection_name="normal_logs",
-    vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
-)
-client.create_collection(
-    collection_name="exception_rules",
-    vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
-)
-client.create_collection(
-    collection_name="anomaly_logs",
-    vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
-)
+
+def create_collection(name, size_of_vector):
+    client.recreate_collection(
+        collection_name=name,
+        vectors_config=VectorParams(size=size_of_vector, distance=Distance.COSINE),
+    )
